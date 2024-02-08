e792e3b6555090d04f6069a9853e2ec68a201e00
RiyaGarhwal <riyagarhwal0612@gmail.com>
2024-02-08 14:51:17 +0530

    done

---
+++ Corejava/src/in/co/rays/oop/Account.java
@@ -0,0 +1,16 @@
+package in.co.rays.oop;
+
+public class Account {
+	private int id;
+	private String firstname;
+	private String lastname;
+	
+	public void setId(int id) {
+		this.id = id;
+		}
+	public int getId() {
+		return id;
+		
+	}
+
+}
---
+++ Corejava/src/in/co/rays/oop/TestAccount.java
@@ -0,0 +1,21 @@
+package in.co.rays.oop;
+
+public class TestAccount {
+	public static void main(String[] args) {
+		Account a=new Account();
+		a.setId(6);
+		System.out.println(a.getId());
+		
+		
+	}
+	
+	
+	
+
+	
+	
+	
+	}
+	
+
+
